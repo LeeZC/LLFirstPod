65c6dd3e0ef17f982e1909c4115fae6c3f94006a
lzc <342502153@qq.com>
2021-11-07 22:39:27 +0800

    添加view。。。

---
+++ Classes/LLView.h
@@ -0,0 +1,16 @@
+//
+//  LLView.h
+//  LLFirstPod
+//
+//  Created by Lees on 2021/11/7.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LLView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Classes/LLView.m
@@ -0,0 +1,30 @@
+//
+//  LLView.m
+//  LLFirstPod
+//
+//  Created by Lees on 2021/11/7.
+//
+
+#import "LLView.h"
+
+@implementation LLView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+-(instancetype)initWithFrame:(CGRect)frame{
+    
+    if(self=[super initWithFrame:frame]){
+        
+        self.backgroundColor=[UIColor yellowColor];
+    }
+    
+    return self;
+}
+
+@end
